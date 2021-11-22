ea5f42cae4bbeebf7e40c737f7b17373e708efad
Josh Uy <angatngheaven@gmail.com>
2021-11-22 21:20:32 +0800

    added tailwind config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 mt-5 bg-green-400'>
+			<div className='flex-shrink-0'>
+				<img src={logo} className='w-25' alt='' />
+			</div>
+			<div>
+				<div class='text-xl font-medium text-black'>
+					Avion School
+				</div>
+				<p class='text-gray-500'>Test</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
