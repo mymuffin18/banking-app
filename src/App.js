48eb61535654db258bb6a6a29d32637532a10e27
Josh Uy <angatngheaven@gmail.com>
2021-11-24 14:08:32 +0800

    added LoginForm component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
 	return (
 		<div class='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4 mt-5 bg-green-400'>
-			<div className='flex-shrink-0'>
-				<img src={logo} className='w-25' alt='' />
-			</div>
-			<div>
-				<div class='text-xl font-medium text-black'>
-					Avion School
-				</div>
-				<p class='text-gray-500'>Test</p>
-			</div>
+			<LoginForm />
 		</div>
 	);
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+	return <h1>Login form</h1>;
+}
+
+export default LoginForm;
