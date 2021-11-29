897ad4f7f9e4a16fafa159e2f07dbc2f8a540ac4
kitkatpussycat <keith.m.pineda@gmail.com>
2021-11-29 11:54:12 +0800

    modified create account

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function CreateAccount() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
     return (
         <>
-          <input type="text" placeholder="email" /><br/>
-          <input type="text" placeholder="password" /> <br/> 
+          <input type="email" placeholder="email" value={email} onChange = {e=>setEmail(e.target.value)} /><br/>
+          <input type="password" placeholder="password" value={password} onChange={e=>setPassword(e.target.value)}/> <br/> 
           <button>Create Account</button>
         </>
     )
