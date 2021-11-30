85451131f4b1ecdb7e78fb0a56f2ab2450e8c69c
kitkatpussycat <keith.m.pineda@gmail.com>
2021-11-30 20:44:34 +0800

    add initial styling whole

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,46 +1,77 @@
-
-import React, { useContext, useState } from 'react';
-import { UserContext } from './context/UserContextProvider';
-import UserList from './UsersList';
-import {useNavigate} from 'react-router-dom'
-import DepositModal from './DepositModal'
-import WithdrawModal from './WithdrawModal'
-import TransferModal from './TransferModal';
+import React, { useContext, useState } from "react";
+import { UserContext } from "./context/UserContextProvider";
+import UserList from "./UsersList";
+import { useNavigate } from "react-router-dom";
+import DepositModal from "./DepositModal";
+import WithdrawModal from "./WithdrawModal";
+import TransferModal from "./TransferModal";
 
 function Dashboard() {
-	const navigate = useNavigate();
-	const [openDepositModal, setOpenDepositModal] = useState(false)
-	const [openWithdrawModal, setOpenWithdrawModal] = useState(false)
-	const [openTransferModal, setOpenTransferModal] = useState(false)
+  const navigate = useNavigate();
+  const [openDepositModal, setOpenDepositModal] = useState(false);
+  const [openWithdrawModal, setOpenWithdrawModal] = useState(false);
+  const [openTransferModal, setOpenTransferModal] = useState(false);
 
-	const { users } = useContext(UserContext);
+  const { users } = useContext(UserContext);
 
+  return (
+    <div className="h-screen">
+      <h1>Dashboard</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="row-span-2 bg-blue-500 card h-60">
+          Container of Info
+        </div>
+        <button
+          className="bg-blue-500"
+          onClick={() => {
+            setOpenDepositModal(true);
+          }}
+        >
+          Deposit
+        </button>
+        {openDepositModal && (
+          <DepositModal closeDepositModal={setOpenDepositModal} />
+        )}
 
-	return (
-		<div className='h-screen'>
-			<div>Dashboard</div>
+        <button
+          className="bg-blue-500"
+          onClick={() => {
+            setOpenWithdrawModal(true);
+          }}
+        >
+          Widthraw
+        </button>
+        {openWithdrawModal && (
+          <WithdrawModal closeWithdrawModal={setOpenWithdrawModal} />
+        )}
 
-			<button onClick={()=>{setOpenDepositModal(true)}}>Deposit</button>
-			{openDepositModal && <DepositModal closeDepositModal={setOpenDepositModal}/>}
+        <button
+          className="bg-blue-500"
+          onClick={() => {
+            setOpenTransferModal(true);
+          }}
+        >
+          Transfer
+        </button>
+        {openTransferModal && (
+          <TransferModal closeTransferModal={setOpenTransferModal} />
+        )}
 
-			<button onClick={()=> {setOpenWithdrawModal(true)}}>Widthraw</button>
-			{openWithdrawModal && <WithdrawModal closeWithdrawModal={setOpenWithdrawModal}/>}
+        <button
+          className="bg-blue-500"
+          onClick={() => {
+            navigate("/create");
+          }}
+        >
+          Create Account
+        </button>
 
-			<button onClick={()=>{setOpenTransferModal(true)}}>Transfer</button>
-			{openTransferModal && <TransferModal closeTransferModal={setOpenTransferModal}/>}
-			
-			<button
-				onClick={() => {
-					navigate('/create');
-				}}
-			>
-				Create Account
-			</button>
+        <button className="bg-blue-500">Log-out</button>
+      </div>
 
-			<UserList />
-		</div>
-	);
-
+      <UserList />
+    </div>
+  );
 }
 
 export default Dashboard;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,54 +1,76 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from './context/AuthContextProvider';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./context/AuthContextProvider";
 function LoginForm({ handleClick }) {
-	const [account, setAccount] = useState({ userName: '', password: '' });
-	const validation = {
-		userName: 'admin',
-		password: 'password',
-	};
+  const [account, setAccount] = useState({ userName: "", password: "" });
+  const validation = {
+    userName: "admin",
+    password: "password",
+  };
 
-	const { dispatch } = useAuth();
+  const { dispatch } = useAuth();
 
-	const navigate = useNavigate();
-	const handleLogin = (e) => {
-		e.preventDefault();
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-		if (
-			account.userName === validation.userName &&
-			account.password === validation.password
-		) {
-			dispatch('LOGIN');
-			navigate('/dashboard', { replace: true });
-		} else {
-			console.log('not matched');
-		}
-	};
+    if (
+      account.userName === validation.userName &&
+      account.password === validation.password
+    ) {
+      dispatch("LOGIN");
+      navigate("/dashboard", { replace: true });
+    } else {
+      console.log("not matched");
+    }
+  };
 
-	return (
-		<form>
-			<h1>Login form</h1>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form className="w-1/6 h-100 p-5 rounded-xl bg-gradient glass card">
+        <h1>Bank Name Log-in</h1>
 
-			<input
-				type='text'
-				value={account.userName}
-				onChange={(e) =>
-					setAccount({ ...account, userName: e.target.value })
-				}
-				placeholder='username'
-			/>
-			<input
-				type='password'
-				value={account.password}
-				onChange={(e) =>
-					setAccount({ ...account, password: e.target.value })
-				}
-				placeholder='password'
-			/>
+        <div className="mb-6">
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            value={account.userName}
+            onChange={(e) =>
+              setAccount({ ...account, userName: e.target.value })
+            }
+            placeholder="username"
+          />
+        </div>
 
-			<button onClick={(e) => handleLogin(e)}>Login</button>
-		</form>
-	);
+        <div className="mb-6">
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            value={account.password}
+            onChange={(e) =>
+              setAccount({ ...account, password: e.target.value })
+            }
+            placeholder="password"
+          />
+        </div>
+
+        <div className="flex items-center justify-between mt-24">
+          <button
+            className="btn-blue focus:outline-none focus:shadow-outline"
+            onClick={(e) => handleLogin(e)}
+          >
+            Login
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
