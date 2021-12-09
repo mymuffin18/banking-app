a8a13d2fe28ee54c916c1896c723608842120147
Josh Uy <angatngheaven@gmail.com>
2021-12-09 18:17:04 +0800

    addsadf

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -4,7 +4,7 @@ function Circles(props) {
 	return (
 		<div className='relative'>
 			<div className='absolute h-36 w-36 bg-pink-500 rounded-full z-0 inset-y-1/2 inset-x-1/2 pink-circle'></div>
-			<div className='absolute h-36 w-36 bg-purple-500 rounded-full z-0 inset-y-1/4 inset-x-1/4 pink-circle'></div>
+			<div className='absolute h-36 w-36 bg-purple-500 rounded-full z-0 inset-y-1/2 inset-x-1/4 pink-circle'></div>
 			{props.children}
 		</div>
 	);
